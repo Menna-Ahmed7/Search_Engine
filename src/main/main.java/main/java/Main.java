bc497b75b87570b2928ac507494fc458b8933d4e
mennamohamed0207 <menamohamed0207@gmail.com>
2023-04-09 12:12:11 +0200

    links

--- src/main/main.java/main/java/Main.java
+++ src/main/main.java/main/java/Main.java
@@ -12,25 +12,26 @@ import org.jsoup.nodes.Element;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         //printing a collection
-//        mongoDB DB1=new mongoDB("DB2");
-//        Iterator it=DB1.getAllk().iterator();
-//        while (it.hasNext()) {
-//            System.out.println(it.next());
-//        }
+        mongoDB DB1=new mongoDB("Seeds");
+        Iterator it=DB1.getAllk().iterator();
+        while (it.hasNext()) {
+
+            System.out.println(it.next());
+        }
         //Seeds
         String Url1="https://www.wikipedia.org/";
         String Url2="https://www.education.com/";
-      //Without Threading
-       // Crawl C1=new Crawl(Url1);
-      //With Threading
-    ThreadedCrawler TC1=new ThreadedCrawler(1,Url1);
+        //Without Threading
+        // Crawl C1=new Crawl(Url1);
+        //With Threading
+        String title = DB1.getLink();
+        ThreadedCrawler TC1=new ThreadedCrawler(1,title);
 
-    ThreadedCrawler TC2=new ThreadedCrawler(2,Url2);
+        ThreadedCrawler TC2=new ThreadedCrawler(2,Url2);
 
-    TC1.Get_thread().join();
-    TC2.Get_thread().join();
+        TC1.Get_thread().join();
+        TC2.Get_thread().join();
 
     }
 
 }
-
---
+++ src/main/main.java/main/java/mongoDB.java
@@ -0,0 +1,90 @@
+package main.java;
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.*;
+import org.bson.Document;
+import java.util.Iterator;
+import java.util.List;
+public class mongoDB {
+    MongoDatabase db;
+    public MongoCollection<Document> k;
+    public MongoDatabase get_db() {
+        return db;
+    }
+    public mongoDB(String Database) { //constructor holds data base name
+
+        try {
+            //---------DataBase Connection
+            //data base connnection string
+            String uri ;
+            if(System.getenv("DB_URI")==null)
+                uri="mongodb://localhost:27017"; //database connection string
+            else
+                uri=System.getenv("DB_URI");
+
+            ConnectionString connection_string = new ConnectionString(uri);
+
+
+            MongoClientSettings settings =
+                    MongoClientSettings.builder().applyConnectionString(connection_string).retryWrites(true).build();
+            //connect to server
+            MongoClient Client = MongoClients.create(settings);
+
+            // Create the data base
+            db = Client.getDatabase(Database);
+
+            System.out.println("successfully connected to data base \n");
+            //---------Collection Creation
+//            db.createCollection("try");
+//            db.createCollection("try1");
+            k = db.getCollection("Links");
+
+            Document document1 = new Document();
+            document1.append("Name", "Wikipedia");
+            document1.append("URL", "https://www.wikipedia.org/");
+            Document document2 = new Document();
+            document2.append("Name", "Education");
+            document2.append("URL", "https://www.education.com/");
+            Document document3 = new Document();
+            document3.append("Name", "Github");
+            document3.append("URL", "https://github.com/");
+            Document document4 = new Document();
+            document4.append("Name", "BBC News");
+            document4.append("URL", " https://www.bbc.com/news");
+            Document document5 = new Document();
+            document5.append("Name", "National Institute of Health");
+            document5.append("URL", " https://www.nih.gov/");
+            Document document6 = new Document();
+            document6.append("Name", "Geeks for Geeks");
+            document6.append("URL", " https://www.geeksforgeeks.org/");
+
+
+            db.getCollection("Links").insertOne(document1);
+            db.getCollection("Links").insertOne(document2);
+            db.getCollection("Links").insertOne(document3);
+            db.getCollection("Links").insertOne(document4);
+            db.getCollection("Links").insertOne(document5);
+            db.getCollection("Links").insertOne(document6);
+        } catch (Exception e) {
+            System.out.println("faild to connect to data base ");
+            e.printStackTrace();
+        }
+
+    }
+    public FindIterable<Document> getAllk() {
+        return k.find(new org.bson.Document());
+    }
+    String getLink ()
+    {
+        // Access the "pages" collection
+        MongoCollection<Document> collection = db.getCollection("Links");
+        // Retrieve the value of the "title" field from the first document in the collection
+        Document firstDocument = collection.find().first();
+        String title = firstDocument.getString("URL");
+        return title;
+    }
+
+
+
+
+}
